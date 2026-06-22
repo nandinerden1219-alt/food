566445b671f432a06ea8ffb2aaf54acde83236b6
nandinerden1219-alt <nandinerden1219@gmail.com>
2026-06-22 13:01:28 +0800

    done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import foodCategoryRoute from "./routes/food-category.route.js";
 
 const app = new Hono();
-
+app.use("*", cors());
 app.route("/category", foodCategoryRoute);
 
 export default app;
